67ecfb2ff2090a69afa20083eccf3717c124b95e
Katherine Bertelsen <kbertelsen@bitwarden.com>
2026-04-14 13:23:26 -0500

    [BWA-99] refactor: Remove Arrange/Act/Assert comments from TOTPServiceTests

--- AuthenticatorShared/Core/Vault/Services/TOTP/TOTPServiceTests.swift
+++ AuthenticatorShared/Core/Vault/Services/TOTP/TOTPServiceTests.swift
@@ -42,15 +42,12 @@ final class TOTPServiceTests: BitwardenTestCase {
     // MARK: Tests
 
     func test_getNextTOTPCode_usesNextPeriodDate() async throws {
-        // Arrange
         let fixedTime = Date(timeIntervalSinceReferenceDate: 1_000_000)
         timeProvider.timeConfig = .mockTime(fixedTime)
         let key = try subject.getTOTPConfiguration(key: .base32Key)
 
-        // Act
         let result = try await subject.getNextTOTPCode(for: key)
 
-        // Assert
         let expectedDate = fixedTime.addingTimeInterval(Double(key.period))
         XCTAssertEqual(result.codeGenerationDate, expectedDate)
     }
