2bffaee90745db49f7e17709c4f3f15c1f760bae
André Bispo <abispo@bitwarden.com>
2026-05-08 19:30:59 +0100

    [PM-36622] fix: alphabetize premiumUpgradeComplete in BillingRoute

--- BitwardenShared/UI/Billing/BillingRoute.swift
+++ BitwardenShared/UI/Billing/BillingRoute.swift
@@ -6,12 +6,12 @@ enum BillingRoute: Equatable {
     /// A route to dismiss the view.
     case dismiss
 
-    /// A route to the premium upgrade complete screen.
-    case premiumUpgradeComplete
-
     /// A route to the premium plan screen.
     case premiumPlan
 
     /// A route to the premium upgrade screen.
     case premiumUpgrade
+
+    /// A route to the premium upgrade complete screen.
+    case premiumUpgradeComplete
 }
